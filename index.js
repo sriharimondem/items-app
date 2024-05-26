4368b5ebdfd19ea431a461a487d1524bdd1f02c9
srihari <srihari.mondem@gmail.com>
2024-05-26 16:03:31 -0500

    updates

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { ObjectId, Int32 } = require("mongodb");
+const axios = require("axios")
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -34,6 +35,7 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+app.use(express.json());
 
 // Routes
 // Define a Mongoose schema for the sample collection (optional)
@@ -68,6 +70,46 @@ app.get("/airbnb", async (req, res) => {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+});
+
+app.post('/api/atlas-access-token', async (req, res) => {
+  try {
+const apiUrl = "https://us-east-1.aws.services.cloud.mongodb.com/api/client/v2.0/app/data-aycdnfw/auth/providers/local-userpass/login";
+
+    // Make a request to Atlas Data API to get the access token
+    const response = await axios.post(apiUrl, {
+      username: "srihari.mondem@gmail.com",
+      password: "mondem",
+    });
+    // Extract the access token from the response
+    const accessToken = response.data.access_token;
+    
+    // Send the access token back to the client
+    res.json( accessToken );
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({error: e.message});
+  }
+}
+)
+
+app.post('/fetchAllUsers', async (req, res)=>{
+  try {
+    
+          
+          const allUsers = await axios.post(
+            "https://us-east-1.aws.data.mongodb-api.com/app/data-aycdnfw/endpoint/data/v1/action/find", req.body.schema,
+            {
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${req.body.token}` 
+            }
+            }
+          )
+          res.json(allUsers.data.documents)
+}catch (e) {
+  console.log(e);
+}
 });
 
 // Error handling middleware
--- src/App.js
+++ src/App.js
@@ -1,87 +1,90 @@
 import "./App.css";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Card, CardActions, CardContent, CardMedia, Button, Typography, CardActionArea, Grid } from '@mui/material';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  CardActionArea,
+  Grid,
+} from "@mui/material";
 
 function App() {
   const [items, setItems] = useState([]);
-  const schema = {collection: "listingsAndReviews",
-  database: "sample_airbnb",
-  dataSource: "GenesisExotics",}
-  const fetchItems = async () => {
-    try {
-      let response = await axios.post(
-        "https://us-east-1.aws.services.cloud.mongodb.com/api/client/v2.0/app/data-aycdnfw/auth/providers/local-userpass/login",
-        {
-          username: "srihari.mondem@gmail.com",
-          password: "mondem",
-        }
-      );
-      let data = JSON.stringify(response);
-      if (data) {
-        let token = JSON.parse(data).data.access_token;
-        await axios.post(
-          "https://us-east-1.aws.data.mongodb-api.com/app/data-aycdnfw/endpoint/data/v1/action/find", schema,
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}` 
-          }
-          }
-        ).then(response => {
-          console.log(response.data);
-          setItems(response.data.documents);
-      })
-      .catch(error => {
-          console.error(error);
-      });;
-      }
-      
-    } catch (e) {
-      throw e;
-    }
+  const [token, setToken] = useState();
+
+  const schema = {
+    collection: "listingsAndReviews",
+    database: "sample_airbnb",
+    dataSource: "GenesisExotics",
+  };
+
+  const getToken = async () => {
+    const key = await axios.post(
+      "http://localhost:3000/api/atlas-access-token"
+    );
+    console.log(key);
+    setToken(key.data);
+  };
+
+  const getAllUsers = async () => {
+    const allUsers = await axios.post(
+      "http://localhost:3000/fetchAllUsers",{
+      token,
+      schema}
+    );
+    setItems(allUsers.data);
   };
 
   useEffect(() => {
-    fetchItems();
-  }, []);
+    if (token) {
+      getAllUsers();
+    } else {
+      getToken();
+    }
+  }, [token]);
 
   return (
     <div className="App">
       <Grid container spacing={2}>
-      {items && items.map((item, index) => 
-      <Grid item xs={6} md={4}>
-      <Card sx={{ margin: 'auto', maxWidth: 600, boxShadow: 3 }} key={index}>
-      <CardActionArea >
-        <CardMedia
-          component="img"
-          height="140"
-          image={item.images.picture_url}
-          alt="Random Unsplash Image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {item.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {item.summary}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-    </Grid>
-    )}
-
+        {items &&
+          items.map((item, index) => (
+            <Grid item xs={6} md={4}>
+              <Card
+                sx={{ margin: "auto", maxWidth: 600, boxShadow: 3 }}
+                key={index}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={item.images.picture_url}
+                    alt="Random Unsplash Image"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {item.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {item.summary}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                  <Button size="small" color="primary">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
       </Grid>
-      
     </div>
   );
 }
